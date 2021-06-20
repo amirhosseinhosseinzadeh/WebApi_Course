74e9fb00b9ea75fdab9dc080e119dfa2ea72e633
Amirhossein <amirhossein.istanbul599@gmail.com>
2021-06-20 22:57:05 +0430

    Config serilog logging system

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using Serilog;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace HotelListing
 {
@@ -14,7 +15,26 @@ namespace HotelListing
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    path:"E:\\Ma3terCloud\\ApiCourse\\SecondTry\\DEVELOP\\HotelListing\\Log",
+                    outputTemplate:"{TimeStamp:yyyy:MM:dd HH:mm:ss.ff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    rollingInterval:RollingInterval.Day
+            ).CreateLogger();
+            try
+            {
+                Log.Information("Applicqation started");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(string.Format("Application stopped:\n{0}",ex.InnerException.Message));
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
